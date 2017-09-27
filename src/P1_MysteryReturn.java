e9e415b3be96cb031ac231d6811c908c0ab6d428
NondescriptArchetype <31418839+NondescriptArchetype@users.noreply.github.com>
2017-09-27 15:29:12 -0500

    Half done

--- src/P1_MysteryReturn.java
+++ src/P1_MysteryReturn.java
@@ -1,21 +1,44 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Mark Kriegsies and Harrison Greer
+	Date: 9/27/2017
 */
 
 /*
  1. Your output prediction (do yourself a favor and go step by step through the code, recording the value of each variable at each method call):
-  
+  z=6
+  1 2 6
+  x=18
+  18 2 3
+  y=12
+  1 12 3
   
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
- 
+ No, we were incorrect we misunderstood how the system would run the program.
  
  */
 public class P1_MysteryReturn {
 
+	public static void main(String[] args) {
+        int x = 1, y = 2, z = 3;
+        z = mystery( x, z, y);
+        System.out.println( x + " " + y + " " + z);
+        x = mystery( z, z, x);
+        System.out.println( x + " " + y + " " + z);
+        y = mystery( y, y, z);
+        System.out.println( x + " " + y + " " + z);
+
+    }
+    
+    public static int mystery( int z, int x, int y) {
+        z--;
+        x = 2 * y + z;
+        y = x - 2;
+        System.out.println(y + " " + z );
+        return x;
+    }
 
 }
--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -1,7 +1,7 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Harrison Greer and Mark Kriegsies
+	Date: 9/27/2017
 */
 
 /*
@@ -15,11 +15,12 @@ public class P4_FixTempConverter {
 		double tempf = 98.6;
 		double tempc = 0.0;
 		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
 	}
 
 	// converts Fahrenheit temperatures to Celsius
-	public static void ftoc(double tempf, double tempc) {
+	public static double ftoc(double tempf, double tempc) {
 	      tempc = (tempf - 32) * 5 / 9;
+	      System.out.println("Body temp in C is: " + tempc);
+	      return tempf;
     }
 }
